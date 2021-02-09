df92ce1d53f5911064faa22169d75f43a7e789e7
MariskaS85 <mcfsnijdewind@gmail.com>
2021-02-09 11:05:21 +0000

    added product views and details

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_products, name='products')
+    path('<product.id>', views.product_detail, name='product_detail')
+]
