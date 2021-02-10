b22852bcf7c88a605cbbd9b3fbf1c11190a7145b
MariskaS85 <mcfsnijdewind@gmail.com>
2021-02-10 15:15:38 +0000

    added quantity control buttons

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,28 +9,8 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    """ access shopping bag session """
     bag = request.session.get('bag', {})
-
-    for item_id, item_data in bag.items():
-        if isinstance(item_data, int):
-            product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
-            product_count += item_data
-            bag_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-            })
-        else:
-            product = get_object_or_404(Product, pk=item_id)
-            for quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
-                product_count += quantity
-                bag_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
     """" A view to return the index page """
 
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    """ This is the session that stores the contents of the bag   """
+    bag = request.session.get('bag', {})
+    """ check if item is in bag,if so, increase quantity else keep quantity """
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else: 
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
+    
