a56c288bc531c58bc39e1cda6f4a5546cd40eee1
MariskaS85 <mcfsnijdewind@gmail.com>
2021-03-01 15:15:42 +0000

    edited customstorages.py

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,10 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3boto3Storage):
+class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
 
-class MediaStorage(S3boto3Storage):
+class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
