f1dac314c06baef26c7ca1ddddc76d89de2487ef
MariskaS85 <mcfsnijdewind@gmail.com>
2021-03-01 15:10:43 +0000

    changed customstorages.py

--- custom_storages.py
+++ custom_storages.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from storages.backends.S3boto3 import S3boto3Storage
+from storages.backends.s3boto3 import S3boto3Storage
 
 
 class StaticStorage(S3boto3Storage):
