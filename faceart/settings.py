c8377be1fc572f56e2ed84b889e598dca81df916
MariskaS85 <mcfsnijdewind@gmail.com>
2021-03-01 13:22:43 +0000

    Edited settings

--- faceart/settings.py
+++ faceart/settings.py
@@ -171,9 +171,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket configuration
     AWS_STORAGE_BUCKET_NAME = 'titik-shop'
-    AWS_s3_REGION_NAME = 'EU (Frankfurt) eu-central-1'
+    AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
